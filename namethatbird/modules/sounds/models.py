1effe3bf022a492bd082b28ce341004c8a23c04a
Alex Marse <alexmarse@Alexs-MacBook-Pro-3.local>
2016-01-07 11:56:15 -0600

    Add Sound/XenoCantoSound models

--- namethatbird/modules/sounds/models.py
+++ namethatbird/modules/sounds/models.py
@@ -2,8 +2,27 @@ import datetime
 from namethatbird.app import db
 
 
-class Sounds(db.Document):
+class XenoCantoSound(db.EmbeddedDocument):
+    id = db.IntField(required=True)
+    genus = db.StringField()
+    species = db.StringField()
+    subspecies = db.StringField()
+    english_name = db.StringField()
+    recordist = db.StringField()
+    country = db.StringField()
+    locality = db.StringField()
+    latitude = db.FloatField()
+    longitude = db.FloatField()
+    sound_type = db.StringField()
+    file_url = db.URLField()
+    file_license = db.StringField()
+    details_url = db.URLField()
+    quality = db.StringField()
+    recorded_at = db.DateTimeField()
+
+
+class Sound(db.Document):
     created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
     updated_at = db.DateTimeField(default=datetime.datetime.now, required=True)
 
-
+    xc_sound = db.EmbeddedDocumentField(XenoCantoSound)
